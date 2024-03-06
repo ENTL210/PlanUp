43c6cf366ecb0f4b7560aada7e1e019ec1965fec
Lam_Edward <lame972@hsd.k12.or.us>
2024-03-06 13:18:51 -0800

    Initialize the basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+
+      </>
+    ),
+  },
+  {
+    path: "/auth",
+    element: (
+      <>
+        Hello
+      </>
+    ),
+  },
+  {
+    path: "calendar",
+    element: (
+      <>
+
+      </>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
