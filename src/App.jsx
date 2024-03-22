46af738d3bcbc8b48a2203853559da40439c757d
ENTL210 <edwardlamhitech@gmail.com>
2024-03-22 14:23:20 -0700

    Working on the main UI

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         </Menu>
         <Calendar>
           Hello
+          
         </Calendar>
       </Main>
     </Container>
@@ -21,3 +22,5 @@ function App() {
 }
 
 export default App
+
+
